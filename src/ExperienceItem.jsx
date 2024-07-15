64c2a57e2538824db510ae6eb80625ca1a7e79ed
jainaashay <jainaashay123@gmail.com>
2024-07-15 17:26:30 +0530

    Added responsiveness for Experience section

--- src/ExperienceItem.jsx
+++ src/ExperienceItem.jsx
@@ -7,14 +7,14 @@ function ExperienceItem(props) {
     <li className="timeline-item bg-white rounded ml-3 p-4 shadow">
                         <div className="timeline-arrow"></div>
                         <div className='row'>
-  <div className='col d-flex flex-column'>
+  <div className='col-10 d-flex flex-column'>
     <h2 className="h5 mb-0" style={{color:'magenta'}}>{props.data.position + ', '+ props.data.company}</h2>
     <span className="small text-primary fw-bold fst-italic">
       <i className="fa fa-clock-o mr-1"></i>{props.data.timeline}
     </span>
   </div>
-  <div className='col'>
-    <img className='mb-0 shadow rounded' src={process.env.PUBLIC_URL + '/images/experience/'+props.data.company+'.jpg'} alt="" style={{width:'4rem',height:'3rem',float:'right'}}/>
+  <div className='col-2'>
+    <img className='mb-0 shadow rounded' src={process.env.PUBLIC_URL + '/images/experience/'+props.data.company+'.jpg'} alt="" style={{width:'3rem',height:'3rem',float:'right'}}/>
   </div>
 </div>
 
