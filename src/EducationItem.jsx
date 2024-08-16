7d075a03d8f58600d028c4f9e18d7de8e16f84e4
jainaashay <jainaashay123@gmail.com>
2024-08-16 23:26:38 +0530

    Improved education-item responsiveness for mobile screens

--- src/EducationItem.jsx
+++ src/EducationItem.jsx
@@ -7,10 +7,10 @@ function EducationItem(props) {
         <img src={process.env.PUBLIC_URL + '/images/education/' + props.data.image + '.jpg'} alt='' className='rounded py-1' style={{ width: '100%', height: '8rem' }} />
       </div>
       <div className='col-md-8 col-12 text-start pb-2'>
-        <div className='fw-bold pt-2' style={{ fontSize: '1.3rem', color: 'aquamarine' }}>
+        <div className='fw-bold pt-2' style={{ fontSize: '1.3rem', color: 'aquamarine', textAlign:'justify' }}>
           {props.data.institute}
         </div>
-        <hr className='text-light fw-bold opacity-100 me-5' />
+        <hr className='text-light fw-bold opacity-100' />
         <span className='text-warning fw-bold pb-2' style={{ fontSize: '1.2rem', color: 'aquamarine' }}>
           {props.data.interval + ' '}
         </span>
