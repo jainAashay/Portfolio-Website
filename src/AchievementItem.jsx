7007c45196dc1d8641f46d60239d4dfd9cb507e9
jainaashay <jainaashay123@gmail.com>
2024-08-17 23:15:42 +0530

    Achivement Item height chnage

--- src/AchievementItem.jsx
+++ src/AchievementItem.jsx
@@ -6,7 +6,7 @@ function AchievementItem(props) {
     return (
         <div className='row shadow rounded my-2 bg-warning' >
             <div className='col-md-4 py-1'>
-                <img src={process.env.PUBLIC_URL + '/images/achievements/' + props.AchievementItem.image} style={{ width: '100%', height: '8rem', maxHeight: '35vh',minHeight:'10vh'}} className='rounded' />
+                <img src={process.env.PUBLIC_URL + '/images/achievements/' + props.AchievementItem.image} style={{ width: '100%', height: '8rem'}} className='rounded' />
             </div>
             <div className='col-md-8 py-1'>
                 <div className="fs-4 pt-1 ps-1 fw-bold" style={{color:'blueviolet'}}>
