13851eb6c3e03ea56515a890db2717e82845c456
jainaashay <jainaashay123@gmail.com>
2024-12-22 11:28:47 +0530

    Added spinner during data fetch

--- src/Components/SchemaManager/SchemaDataView.jsx
+++ src/Components/SchemaManager/SchemaDataView.jsx
@@ -7,7 +7,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
 import FilterBar from './FilterBar';
 
-
 function SchemaDataView() {
     const { schema } = useParams();
     const [message, setMessage] = useState('');
@@ -18,6 +17,7 @@ function SchemaDataView() {
     const [filterParams, setFilterParams] = useState({});
     const [pageNumber, setPageNumber] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
+    const [loading, setLoading] = useState(false); // State for loading spinner
 
     const handleFilterApply = (newFilterPayload) => {
         setFilterParams(newFilterPayload); // Update filterParams when filters are applied
@@ -25,6 +25,8 @@ function SchemaDataView() {
 
     useEffect(() => {
         async function fetchSchemaData() {
+            setLoading(true); // Show spinner
+            setMessage('');   // Clear previous messages
             try {
                 const loginToken = Cookies.get('login_token');
                 const response = await axios.post(backend_endpoint + '/schema/' + schema + '/view',
@@ -38,114 +40,104 @@ function SchemaDataView() {
                         }
                     });
 
-                if (response.status == 200) {
-
+                if (response.status === 200) {
                     setData(response.data.data);
-                    setKeys(response.data.keys.filter(key => key != '_id'));
+                    setKeys(response.data.keys.filter(key => key !== '_id'));
                     setTotalPages(Math.ceil(response.data.total_count / 20));
                     setFilters(response.data.filters);
-                    setMessage('');
+                } else {
+                    setMessage(response.data.message || 'Failed to fetch data.');
                 }
-                else {
-                    setMessage(response.data.message);
-                }
-
             } catch (error) {
-                setMessage('An error displaying the data. Please try again !');
+                setMessage('An error occurred while fetching data. Please try again.');
                 console.error(error);
+            } finally {
+                setLoading(false); // Hide spinner
             }
-
         }
         fetchSchemaData();
-    }, [filterParams, pageNumber])
+    }, [filterParams, pageNumber]);
 
     function toggleSidebarVisibility() {
         setVisible(!visible);
     }
 
-    function handlePageClick() {
-        setPageNumber(pageNumber)
-    }
-
     return (
         <div style={{ backgroundColor: 'skyblue' }}>
-
             <FilterBar filters={filters} isVisible={visible} onClose={() => setVisible(false)} onApply={handleFilterApply} />
-                
+
             <div className="container pt-4 responsive-container" style={{ width: '80%' }}>
                 <div className='py-2 text-center'>
                     <h1 className='fw-bold pb-2 text-danger'>{schema}</h1>
                 </div>
-                <div className='pb-2'>
-                    <div className='fst-italic fw-bold'>{message}</div>
-                    <FontAwesomeIcon icon={faFilter} style={{ fontSize: '1.5rem', cursor: 'pointer' }} className='float-end text-danger' onClick={toggleSidebarVisibility} />
-                </div>
-                <div className='mt-4 pb-4'>
-                    <div className='table-responsive text-light' style={{ maxHeight: '100vh' }}>
 
-                        <table className='text-center' style={{ width: '100%', minWidth: '400px', margin: '0 auto', tableLayout: 'fixed' }}>
-                            <thead className='position-sticky top-0'>
-                                <tr >
-                                    {
-                                        keys.map((item, index) => (
-                                            <th className='fw-bold bg-warning text-dark' key={index} style={{ backgroundColor: 'yellow' }}>
-                                                {item}
-                                            </th>
-                                        ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    data.map((item, index) => (
-                                        <tr key={index}>
-                                            {keys.map((key, idx) => (
-                                                <td className='bg-dark' key={idx} style={{ backgroundColor: 'bisque' }}>{item[key]}</td>  // Ensure this line returns the <td> element
+                {loading ? ( // Spinner while loading
+                    <div className="text-center py-5">
+                        <div className="spinner-border" role="status">
+                            <span className="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                ) : message ? ( // Error message
+                    <div className='text-center py-3 text-dark fst-italic'>
+                        <strong>{message}</strong>
+                    </div>
+                ) : (
+                    <>
+                        <div className='pb-2'>
+                            <FontAwesomeIcon icon={faFilter} style={{ fontSize: '1.5rem', cursor: 'pointer' }} className='float-end text-danger' onClick={toggleSidebarVisibility} />
+                        </div>
+                        <div className='mt-4 pb-4'>
+                            <div className='table-responsive text-light' style={{ maxHeight: '100vh' }}>
+                                <table className='text-center' style={{ width: '100%', minWidth: '400px', margin: '0 auto', tableLayout: 'fixed' }}>
+                                    <thead className='position-sticky top-0'>
+                                        <tr>
+                                            {keys.map((item, index) => (
+                                                <th className='fw-bold bg-warning text-dark' key={index} style={{ backgroundColor: 'yellow' }}>
+                                                    {item}
+                                                </th>
                                             ))}
                                         </tr>
-                                    ))
-                                }</tbody>
-                        </table>
-                    </div>
-                </div>
-
-                <div className='pb-4'>
-                    <div className='pb-5'>
-                        <nav className='float-end'>
-                            <ul className="pagination">
-                                <li className="page-item">
-                                    <button className="page-link" onClick={() => setPageNumber(pageNumber => Math.max(pageNumber - 1, 1))}>
-                                        Previous
-                                    </button>
-                                </li>
-
-                                {
-                                    Array.from({ length: Math.min(10, totalPages) }, (_, i) => i + 1).map((page) => (
+                                    </thead>
+                                    <tbody>
+                                        {data.map((item, index) => (
+                                            <tr key={index}>
+                                                {keys.map((key, idx) => (
+                                                    <td className='bg-dark' key={idx} style={{ backgroundColor: 'bisque' }}>{item[key]}</td>
+                                                ))}
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div className='pb-4'>
+                            <nav className='float-end'>
+                                <ul className="pagination">
+                                    <li className="page-item">
+                                        <button className="page-link" onClick={() => setPageNumber(pageNumber => Math.max(pageNumber - 1, 1))}>
+                                            Previous
+                                        </button>
+                                    </li>
+                                    {Array.from({ length: Math.min(10, totalPages) }, (_, i) => i + 1).map((page) => (
                                         <li key={page} className={`page-item ${pageNumber === page ? 'active' : ''}`}>
                                             <button className="page-link" onClick={() => setPageNumber(page)}>
                                                 {page}
                                             </button>
                                         </li>
-                                    ))
-                                }
-
-                                <li className="page-item">
-                                    <button className="page-link" onClick={() => setPageNumber(prev => Math.min(pageNumber + 1, totalPages))}>
-                                        Next
-                                    </button>
-                                </li>
-
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-
-
-
-
+                                    ))}
+                                    <li className="page-item">
+                                        <button className="page-link" onClick={() => setPageNumber(prev => Math.min(pageNumber + 1, totalPages))}>
+                                            Next
+                                        </button>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </>
+                )}
             </div>
-
         </div>
-    )
+    );
 }
 
 export default SchemaDataView;
